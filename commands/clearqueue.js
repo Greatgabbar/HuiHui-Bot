bcea026627765a4180157e828aa3acf926f917dd
GreatGabbar <strivedi_be19@thapar.edu>
2021-03-16 12:57:13 +0530

    clearqueue command added

---
+++ commands/clearqueue.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+
+module.exports={
+    name : 'clearqueue',
+    discription : 'Clear the queue',
+    usage : "!clearqueue or !cq",
+    aliases:["cq"],
+    execute(message,args){
+        if (!message.member.voice.channel) {
+            return message.reply(`You Have to be in the voice channel to use this command !`);
+       }
+    //    if(message.member.voice.channel!==message.client.voice.channel){
+    //     return message.reply(`You Have to be in the same voice channel of the bot`);
+    //    }
+    if(!message.guild.me.voice.channel){
+        return message.reply(`I am not in any Voice channel use !play to invite me!`);
+    }
+
+    message.client.player.clearQueue(message);
+        const msg=new Discord.MessageEmbed()
+            .setColor('#FFA500')
+            .setTitle(`Queue Cleared Successfully`)
+        return message.channel.send(msg).catch(err=>{
+            console.log('Err',err.message);
+           return message.reply(`Reason : ${err.message}`)
+        });
+
+    }
+}
