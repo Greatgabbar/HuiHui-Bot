a0d19396763c864be774e6673e7a8a31e27d4abf
GreatGabbar <strivedi_be19@thapar.edu>
2021-03-16 11:34:43 +0530

    9th try

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
   apps : [{
       name : 'app',
     script: 'app.js',
-    instances: "max",
     env: {
         NODE_ENV: "development",
         CLIENTID:"816994233557844039",
