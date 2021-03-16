7b85930c7e854632a937f69ded29a422981d020b
GreatGabbar <strivedi_be19@thapar.edu>
2021-03-16 09:40:23 +0530

    deploy try -6

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
         CLIENTID:"816994233557844039",
         CLIENTSECRET : "YDz4kgQmNWax3F82GlYTvbc0Bew5ZBgh",
         TOKEN : "ODE2OTk0MjMzNTU3ODQ0MDM5.YEDDLA.wg_UiwM2V96hPQxYX2cJPzd0lJs",
-        dburl = 'mongodb+srv://root:9755@cluster0.qsytw.mongodb.net/HuiHui?retryWrites=true&w=majority',
+        dburl : 'mongodb+srv://root:9755@cluster0.qsytw.mongodb.net/HuiHui?retryWrites=true&w=majority',
         prefix:"!"
     },
     env_production: {
@@ -16,7 +16,7 @@ module.exports = {
         CLIENTID:"816994233557844039",
         CLIENTSECRET : "YDz4kgQmNWax3F82GlYTvbc0Bew5ZBgh",
         TOKEN : "ODE2OTk0MjMzNTU3ODQ0MDM5.YEDDLA.wg_UiwM2V96hPQxYX2cJPzd0lJs",
-        dburl = 'mongodb+srv://root:9755@cluster0.qsytw.mongodb.net/HuiHui?retryWrites=true&w=majority',
+        dburl : 'mongodb+srv://root:9755@cluster0.qsytw.mongodb.net/HuiHui?retryWrites=true&w=majority',
         prefix:"!"
     }
   }]
