31d502769e21e8c5bdc03b7eaf5422e72c293763
GreatGabbar <strivedi_be19@thapar.edu>
2021-03-23 01:04:45 +0530

    puppeteer error fix trail :1

--- util/lyrics.js
+++ util/lyrics.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
-const $=require('cheerio');
 module.exports = async (title)=>{
-    const browser= await puppeteer.launch();
+    const browser= await puppeteer.launch({
+        args: ['--no-sandbox']
+    });
     const page= await browser.newPage();
     await page.setViewport({
         width: 1920,
